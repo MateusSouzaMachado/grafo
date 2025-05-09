163c92d16ec7e89108def1b24aebb48c170bd8bc
Mateus Souza Machado <mateus2005machado@gmail.com>
2025-05-08 21:40:19 -0300

    1

--- demo/src/main/java/com/example/Pratica.java
+++ demo/src/main/java/com/example/Pratica.java
@@ -1,50 +1,183 @@
 package com.example;
 
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Pratica {
     public static double calcularAumento(double salario) {
         double aumento;
         if (salario < 1500) {
-        aumento = salario * 0.15;
+            aumento = salario * 0.15;
         } else if (salario >= 1500 && salario <= 3000) {
-        aumento = salario * 0.12;
+            aumento = salario * 0.12;
         } else {
-        aumento = salario * 0.07;
+            aumento = salario * 0.07;
         }
         return aumento;
     }
 
     public static boolean verificarPrimo(int num) {
         if (num <= 1) {
-        return false;
+            return false;
         }
         for (int i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i == 0) {
-        return false;
-        }
+            if (num % i == 0) {
+                return false;
+            }
         }
         return true;
     }
 
     public static int calcularMDC(int a, int b) {
         while (b != 0) {
-        int temp = b;
-        b = a % b;
+            int temp = b;
+            b = a % b;
             a = temp;
         }
-               
+
         return a;
     }
 
     public static String classificarAluno(double nota) {
         String classificacao;
         if (nota < 5) {
-        classificacao = "Reprovado";
+            classificacao = "Reprovado";
         } else if (nota >= 5 && nota < 7) {
-        classificacao = "Recuperação";
+            classificacao = "Recuperação";
         } else {
-        classificacao = "Aprovado";
+            classificacao = "Aprovado";
         }
         return classificacao;
     }
+
+    public boolean calculaMediaFinal(double notaA, double notaB, double exame) {
+        double media = (notaA + notaB) / 2;
+        boolean aprovado = false;
+
+        if (media >= 6) {
+            aprovado = true;
+        } else {
+            double mediaExame = (media * 1.7) + (exame * 1.3);
+            if (mediaExame >= 14) {
+                aprovado = true;
+            } else {
+                aprovado = false;
+            }
+        }
+
+        return aprovado;
+    }
+
+    public static int calcularSoma(int n) {
+        int soma = 0;
+        for (int i = 1; i <= n; i++) {
+            soma += i;
+        }
+        return soma;
+    }
+
+    public static int calcularFatorial(int n) {
+        int resultado = 1;
+        for (int i = 1; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
+
+    public static double calcularDesconto(double valorCompra) {
+        double desconto;
+        if (valorCompra < 50) {
+            desconto = 0;
+        } else if (valorCompra >= 50 && valorCompra < 100) {
+            desconto = valorCompra * 0.05;
+        } else {
+            desconto = valorCompra * 0.1;
+        }
+        return desconto;
+    }
+
+    public static double calcularDesconto2(int valorCompra) {
+        double desconto;
+
+        switch (valorCompra / 10) {
+            case 0:
+                desconto = 0;
+                break;
+            case 1:
+                desconto = 1;
+                break;
+            case 2:
+                desconto = 2;
+                break;
+            case 3:
+                desconto = 4;
+                break;
+            case 4:
+                desconto = 5;
+                break;
+            case 5:
+                desconto = 6;
+                break;
+            case 6:
+                desconto = 8;
+                break;
+            case 7:
+                desconto = 9;
+                break;
+            case 8:
+                desconto = 9;
+                break;
+            case 9:
+                desconto = valorCompra * 0.05;
+                break;
+            default:
+                desconto = valorCompra * 0.1;
+                break;
+        }
+        return desconto;
+    }
+
+    public static boolean isCPFValido(String cpf) {
+        cpf = cpf.replaceAll("[^0-9]", "");
+        if (cpf.length() != 11)
+            return false;
+        if (cpf.matches("(\\d)\\1{10}"))
+            return false;
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += Integer.parseInt(cpf.substring(i, i + 1)) * (10 -
+                    i);
+        }
+        int digito1 = 11 - (soma % 11);
+        if (digito1 > 9)
+            digito1 = 0;
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += Integer.parseInt(cpf.substring(i, i + 1)) * (11 -
+                    i);
+        }
+        int digito2 = 11 - (soma % 11);
+        if (digito2 > 9)
+            digito2 = 0;
+        return Integer.parseInt(cpf.substring(9, 10)) == digito1 &&
+                Integer.parseInt(cpf.substring(10, 11)) == digito2;
+    }
+
+     public static LocalDateTime validarData(String dataString) {
+        try {
+            // Definindo o formato da data
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
             
+            // Convertendo a string para LocalDateTime
+            LocalDateTime data = LocalDateTime.parse(dataString, formatter);
+            
+            return data;
+        } catch (DateTimeException e) {
+            // Se ocorrer uma exceção ao fazer o parsing da data
+            // significa que a data não está em um formato válido
+            System.out.println("Erro ao validar data: " + e.getMessage());
+            return null;
+        }
+    }
 }
--- demo/src/test/java/PraticaTest.java
+++ demo/src/test/java/PraticaTest.java
@@ -1,6 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +29,68 @@ public class PraticaTest {
     public void caso3(){
         assertEquals(Pratica.calcularMDC(2, 2), 2);
     }
+
+    @Test
+    public void caso4(){
+        assertEquals(Pratica.classificarAluno(4), "Reprovado");
+        assertEquals(Pratica.classificarAluno(6), "Recuperação");
+        assertEquals(Pratica.classificarAluno(8), "Aprovado");
+    }
+
+    @Test
+    public void caso5(){
+        Pratica p = new Pratica();
+        assertTrue(p.calculaMediaFinal(7, 8, 6));
+        assertFalse(p.calculaMediaFinal(4, 5, 2));
+        assertTrue(p.calculaMediaFinal(4, 5, 10));
+    }
+
+    @Test
+    public void caso6(){
+        assertEquals(Pratica.calcularSoma(2), 3);
+    }
+    
+    @Test
+    public void caso7(){
+        assertEquals(Pratica.calcularFatorial(5), 120);
+    }
+
+    @Test
+    public void caso8(){
+        assertEquals(Pratica.calcularDesconto(20), 0);
+        assertEquals(Pratica.calcularDesconto(50), 2.50);
+        assertEquals(Pratica.calcularDesconto(100), 10);
+    }
+
+    @Test
+    public void caso9(){
+        assertEquals(Pratica.calcularDesconto2(1), 0);
+        assertEquals(Pratica.calcularDesconto2(10), 1);
+        assertEquals(Pratica.calcularDesconto2(20), 2);
+        assertEquals(Pratica.calcularDesconto2(30), 4);
+        assertEquals(Pratica.calcularDesconto2(40), 5);
+        assertEquals(Pratica.calcularDesconto2(50), 6);
+        assertEquals(Pratica.calcularDesconto2(60), 8);
+        assertEquals(Pratica.calcularDesconto2(70), 9);
+        assertEquals(Pratica.calcularDesconto2(80), 9);
+        assertEquals(Pratica.calcularDesconto2(90), 4.5);
+        assertEquals(Pratica.calcularDesconto2(100), 10);
+    }
+
+    @Test
+    public void casoDate(){
+        assertNull(Pratica.validarData("2023-14-01 20:00:00"));
+        assertNull(Pratica.validarData("2023-01-32 20:00:00"));
+        assertNull(Pratica.validarData("2023-01-01 25:00:00"));
+        assertNull(Pratica.validarData("2023-01-01 20:60:00"));
+        assertNull(Pratica.validarData("2023-01-01 20:00:60"));
+        assertNull(Pratica.validarData("2023-01-01 20:00:00.000"));
+        assertNotNull(Pratica.validarData("2054-01-01 20:00:00"));
+        assertNull(Pratica.validarData("02-05-1524 20:00:00"));
+        assertNotNull(Pratica.validarData("2023-01-01 20:00:00"));
+        assertEquals(Pratica.validarData("2023-01-01 20:00:00"),LocalDateTime.of(2023, 1, 1, 20, 0, 0));
+    }
+
+   
 
 }
